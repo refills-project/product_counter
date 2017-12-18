9c433bb858a76de7fbce6c2d0410ef118f68f847
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2017-12-18 14:49:00 +0100

    transform listener

--- src/counter_node.cpp
+++ src/counter_node.cpp
@@ -1,5 +1,7 @@
 #include "ros/ros.h"
 #include "tf/tf.h"
+#include "tf/transform_listener.h"
+#include "tf/transform_broadcaster.h"
 
 
 #include "refills_msgs/CountObjects.h"
@@ -12,6 +14,8 @@ class CounterInterface
   RealSenseBridge cameraBridge_;
   ros::ServiceServer service;
 
+  tf::TransformListener _tfListener;
+  tf::TransformBroadcaster _tfBroadcaster;
   counter productCounter;
 
   bool first;
@@ -31,32 +35,65 @@ public:
       usleep(100);
     }
     ROS_INFO("Got data");
- 
+
     cv::Mat rgb, depth;
     sensor_msgs::CameraInfo cam_info;
     cameraBridge_.getData(rgb, depth, cam_info);
-    ROS_INFO("Image size received: [%d * %d] ",rgb.cols,rgb.rows);	
+    ROS_INFO("Image size received: [%d * %d] ", rgb.cols, rgb.rows);
     if(first)
     {
       //set camera params in productCounter
-      first=false;
+      first = false;
     }
 
+    geometry_msgs::PoseStamped poseMsgInCameraFrame;
+
+
+    tf::StampedTransform mapToCam,camToSeparator;
+    _tfListener.lookupTransform("rs_camera_depth_optical_frame","map_rotated",ros::Time::now(),mapToCam);
+
+    if(req.separator_location.header.frame_id != "map_rotated")
+    {
+      _tfListener.transformPose("rs_camera_depth_optical_frame",
+                                req.separator_location, poseMsgInCameraFrame);
+    }
+    else
+    {
+      poseMsgInCameraFrame = req.separator_location;
+    }
     tf::Stamped<tf::Pose> poseStamped;
-    tf::poseStampedMsgToTF(req.separator_location,poseStamped);
-    tf::Matrix3x3 rotMat = poseStamped.getBasis();
+    tf::poseStampedMsgToTF(poseMsgInCameraFrame, poseStamped);
+
+    camToSeparator.child_frame_id_ = "separator_1";
+    camToSeparator.frame_id_= "rs_camera_depth_optical_frame";
+    camToSeparator.setOrigin(poseStamped.getOrigin());
+    camToSeparator.setRotation(mapToCam.getRotation());
+    camToSeparator.stamp_ = ros::Time::now();
+
+    _tfBroadcaster.sendTransform(camToSeparator);
+
+    tf::Matrix3x3 rotMat = mapToCam.getBasis();
     cv::Mat cvRot(3, 3, CV_64F);
-    cvRot.at<double>(0,0) = rotMat[0][0]; cvRot.at<double>(0,1) = rotMat[0][1]; cvRot.at<double>(0,2) = rotMat[0][2];
-    cvRot.at<double>(1,0) = rotMat[1][1]; cvRot.at<double>(1,1) = rotMat[1][1]; cvRot.at<double>(1,2) = rotMat[1][2];
-    cvRot.at<double>(2,0) = rotMat[2][1]; cvRot.at<double>(2,1) = rotMat[2][1]; cvRot.at<double>(2,2) = rotMat[2][2];
+    cvRot.at<double>(0, 0) = rotMat[0][0];
+    cvRot.at<double>(0, 1) = rotMat[0][1];
+    cvRot.at<double>(0, 2) = rotMat[0][2];
+    cvRot.at<double>(1, 0) = rotMat[1][1];
+    cvRot.at<double>(1, 1) = rotMat[1][1];
+    cvRot.at<double>(1, 2) = rotMat[1][2];
+    cvRot.at<double>(2, 0) = rotMat[2][1];
+    cvRot.at<double>(2, 1) = rotMat[2][1];
+    cvRot.at<double>(2, 2) = rotMat[2][2];
+
     productCounter.setRotation(cvRot);
     productCounter.setImg(depth);
+
     cv::Point3d separatorPosition;
     separatorPosition.x = poseStamped.getOrigin().x();
     separatorPosition.y = poseStamped.getOrigin().y();
     separatorPosition.z = poseStamped.getOrigin().z();
+
     productCounter.setSeparator(separatorPosition);
-    productCounter.setType(req.obj_width,req.obj_height,req.obj_depth,req.object_name);
+    productCounter.setType(req.obj_width, req.obj_height, req.obj_depth, req.object_name);
 
     res.object_count = productCounter.countObjects();
     ROS_INFO("sending back response: [%ld]", (long int)res.object_count);
